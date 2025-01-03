bbbe53067a9e7a0ed50b734ade644ee24693f02a
Hamza <hamzahalam2001@proton.me>
2025-01-03 22:18:02 +0500

    Initial Commit

---
+++ GarageLink/GarageLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarageLinkApp.swift
+//  GarageLink
+//
+//  Created by Hamza Hashmi on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GarageLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
